78ba740abe3cc035063512dad772dd35f25cbb90
rajat503 <rajat503@gmail.com>
2016-11-13 19:01:52 +0530

    renamed files

--- cnn/iou.py
+++ cnn/iou.py
@@ -46,7 +46,7 @@ def train(user_list):
     return test_boxes
 
 L1 = np.load("output.txt.npy")
-l1 = L1.astype(int)
+L1 = L1.astype(int)
 N = len(L1)
 L2 = train(['user_3','user_4', 'user_5','user_6','user_7','user_9','user_10', 'user_11', 'user_12', 'user_13', 'user_14' ,'user_15', 'user_16', 'user_17', 'user_18','user_19'])
 
